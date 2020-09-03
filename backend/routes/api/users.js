01d351e2694d3e82d6f197856b03ba9a072b9911
Elaine <elaine@Felipes-iMac.local>
2020-09-03 15:58:25 -0500

    [users] add /login route and test

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -9,6 +9,7 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 //load user model
 const User = require('../../models/User');
+const { db } = require('../../models/User');
 
 //Get api user
 router.get('/test', (req,res) =>{
@@ -40,6 +41,40 @@ router.post('/register', (req, res) =>{
                     .then(user => res.json(user))
                     .catch(error => console.log(error))
                 })
+            })
+        }
+    })
+})
+
+router.post('/login', (req, res) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find a user via email
+    User.findOne({ email})
+    .then(user =>{
+        if (!user){
+            res.status(400).json({msg: 'User not found!!!'})
+        } else {
+            //check password with bcrypt
+            bcrypt.compare(password, user.password)
+            .then(isMatch =>{
+                if (isMatch) {
+                    //user match, send JSON web token
+                    //create token payload (you can include anything you want)
+                    const payload = {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email
+                    }
+                    //sign token
+                    jwt.sign(payload, JWT_SECRET, { expiresIn: 3600 }, (error, token) =>{
+                        res.json({success: true, token: `Bearer ${token}`})
+                    })
+                } else {
+                    return res.status(400).json({ password: 'Password or email is incorrect' })
+
+                }
             })
         }
     })
